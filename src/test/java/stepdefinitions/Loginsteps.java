718f88d7f7e233b1f1fa623256d888b04a9e3cb9
SreeHemalatha <lathabhj@gmail.com>
2024-03-01 21:38:58 -0600

    updated dependencies

--- src/test/java/stepdefinitions/Loginsteps.java
+++ src/test/java/stepdefinitions/Loginsteps.java
@@ -1,11 +1,12 @@
 package stepdefinitions;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 public class Loginsteps {
 	WebDriver driver=null;
--- src/test/java/stepdefinitions/Testrunner.java
+++ src/test/java/stepdefinitions/Testrunner.java
@@ -1,8 +1,16 @@
 package stepdefinitions;
 import org.junit.runner.RunWith;
-import io.cucumber.junit.cucumber;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
 @RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\resources\\features\\login.feature",glue=
+{"src\\test\\java\\stepdefinitions\\Loginsteps.java"},
+monochrome=true,
+plugin= {"pretty","junit:target/JUnitReports/report.xml",
+"json:target/JSONreport/report.json",
+"html:target/HtmlReports"},
+tags="@snitytest")
 public class Testrunner {
 
 }
