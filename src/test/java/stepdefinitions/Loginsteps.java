76c38f9dfe51c91e145e22e7d710a582091eb213
hemabh918 <67938230+hemabh918@users.noreply.github.com>
2024-03-02 10:40:22 -0700

    Update Loginsteps.java

--- src/test/java/stepdefinitions/Loginsteps.java
+++ src/test/java/stepdefinitions/Loginsteps.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 public class Loginsteps {
 	WebDriver driver=null;
-	@Given("browser is open")
+	@Given("browser is open.")
 	public void browser_is_open() {
 	 System.out.println("Inside step - browser is open ");
 	 System.setProperty("webdriver.chrome.driver","D:\\Testing_training\\java-selenium\\cucumber2\\target\\drivers\\chromedriver.exe");
@@ -21,20 +21,32 @@ public class Loginsteps {
 			 driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
 
 			 }
-			 @And("user is on demoblaze page")
+			 @And("user is on demoblaze page.")
 			 public void user_is_on_demoblaze_page() {
 			  driver.navigate().to("https://www.demoblaze.com/");
 			 }
-			 @And("user clicks on login")
+			 @And("click on login button.")
 			 public void user_clicks_on_login() {
 			 driver.findElement(By.id("login2")).click();
             }
-			 @When("user enters \"(.*)\" and \"(.*)")
-			 public void user_enters_username_and_password(String username,String password) throws InterruptedException 
+			 @When("user enters {word} and {word}")
+			public void user_enters_username_and_password(String username,String password) throws InterruptedException 
 			  {
 			  driver.findElement(By.id("loginusername")).sendKeys(username);
-			  driver.findElement(By.id("password")).sendKeys(password);
+			  driver.findElement(By.id("loginpassword")).sendKeys(password);
 			 Thread.sleep(2000);
-			 driver.close();
-			 driver.quit();
-}}
+}
+    @And("user clicks on login.")
+    public void user_clicks_on_login() {
+        //btn btn-primary
+       driver.findElement(By.cssSelector("btn.btn-primary")).click();
+        driver.close();
+        driver.quit();
+    }
+
+    @Then("error message displays.")
+    public void error_message_displays() {
+        System.out.println("Inside Step - user is navigated to demoblaze home");
+    }
+
+}
