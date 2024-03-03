35add9544fd8ceb9cd4d7b7a7178fc21ec0052f5
Satya Hemalatha Bhavaraju <hema@Satyas-MacBook-Pro.local>
2024-03-03 07:06:01 -0700

    code formatted

--- src/test/java/stepdefinitions/Testrunner.java
+++ src/test/java/stepdefinitions/Testrunner.java
@@ -1,16 +1,17 @@
 package stepdefinitions;
+
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\features",glue=
-{"src\\test\\java\\stepdefinitions"},
-monochrome=true,
-plugin= {"pretty","junit:target/JUnitReports/report.xml",
-"json:target/JSONreport/report.json",
-"html:target/HtmlReports"},
-tags="@snitytest")
+@CucumberOptions(features = "src\\test\\resources\\features", glue =
+        {"src\\test\\java\\stepdefinitions"},
+        monochrome = true,
+        plugin = {"pretty", "junit:target/JUnitReports/report.xml",
+                "json:target/JSONreport/report.json",
+                "html:target/HtmlReports"},
+        tags = "@snitytest")
 public class Testrunner {
 
 }
