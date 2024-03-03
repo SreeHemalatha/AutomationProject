7a4a2b855c0d83ef3b0386ef4b6f3adbe57c5ebb
Satya Hemalatha Bhavaraju <hema@Satyas-MacBook-Pro.local>
2024-03-03 06:51:24 -0700

    Code formatted

--- src/test/java/stepdefinitions/Loginsteps.java
+++ src/test/java/stepdefinitions/Loginsteps.java
@@ -1,6 +1,8 @@
 package stepdefinitions;
+
 import java.util.concurrent.TimeUnit;
 
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,38 +10,43 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+
 public class Loginsteps {
-	WebDriver driver=null;
-	@Given("browser is open.")
-	public void browser_is_open() {
-	 System.out.println("Inside step - browser is open ");
-	 System.setProperty("webdriver.chrome.driver","D:\\Testing_training\\java-selenium\\cucumber2\\target\\drivers\\chromedriver.exe");
-			  driver = new ChromeDriver();
+    WebDriver driver = null;
 
-			 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+    @Given("browser is open.")
+    public void browser_is_open() {
+        System.out.println("Inside step - browser is open ");
+        System.setProperty("webdriver.chrome.driver", "D:\\Testing_training\\java-selenium\\cucumber2\\target\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
 
-			 driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-			 }
-			 @And("user is on demoblaze page.")
-			 public void user_is_on_demoblaze_page() {
-			  driver.navigate().to("https://www.demoblaze.com/");
-			 }
-			 @And("click on login button.")
-			 public void user_clicks_on_login() {
-			 driver.findElement(By.id("login2")).click();
-            }
-			 @When("user enters {word} and {word}")
-			public void user_enters_username_and_password(String username,String password) throws InterruptedException 
-			  {
-			  driver.findElement(By.id("loginusername")).sendKeys(username);
-			  driver.findElement(By.id("loginpassword")).sendKeys(password);
-			 Thread.sleep(2000);
-}
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+    }
+
+    @And("user is on demoblaze page.")
+    public void user_is_on_demoblaze_page() {
+        driver.navigate().to("https://www.demoblaze.com/");
+    }
+
+    @And("click on login button.")
+    public void clicks_on_login_button() {
+        driver.findElement(By.id("login2")).click();
+    }
+
+    @When("user enters {word} and {word}")
+    public void user_enters_username_and_password(String username, String password) throws InterruptedException {
+        driver.findElement(By.id("loginusername")).sendKeys(username);
+        driver.findElement(By.id("loginpassword")).sendKeys(password);
+        Thread.sleep(2000);
+    }
+
     @And("user clicks on login.")
     public void user_clicks_on_login() {
         //btn btn-primary
-       driver.findElement(By.cssSelector("btn.btn-primary")).click();
+        driver.findElement(By.cssSelector("btn.btn-primary")).click();
         driver.close();
         driver.quit();
     }
